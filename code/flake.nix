08a93adcea7412878bdab8f41b6be0840189188a
alf171 <alfonso.laffont@gmail.com>
2026-07-02 00:22:16 -0400

    fix(nix): commit flake

---
+++ code/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "CMU Compilers development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            zig
+            python313
+            pkg-config
+          ];
+        };
+      });
+    };
+}
